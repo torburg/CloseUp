4b45299aa582077538c587e2265d8eeeaf362874
torburg <torburg@yandex.ru>
2020-03-02 20:21:21 +0300

    Added Background to Main Scree and gitignore

--- CloseUp/ViewController.swift
+++ CloseUp/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(#function)
+        
+        setView()
+        if true {
+            label.text = "TRUE"
+            label.textColor = .black
+        }
     }
 
-
+    func setView() {
+        
+        let gradient = Background().gradient
+        gradient.frame = view.bounds
+        self.view.layer.insertSublayer(gradient, at: 0)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        let constraints = [
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
 }
+
+class Background {
+    
+    let gradient = CAGradientLayer()
+    
+    init() {
+        let topColor = UIColor(red: 68.0 / 255.0, green: 168.0 / 255.0, blue: 238.0 / 255.0, alpha: 1)
+        let bottomColor = UIColor(red: 168.0 / 255.0, green: 178.0 / 255.0, blue: 200.0 / 255.0, alpha: 1)
+        gradient.colors = [topColor, bottomColor]
+//        gradient.locations = [0.0, 1.0]
+    }
+}
+
 
